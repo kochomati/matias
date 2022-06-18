5c62c05cae22900e654fa0beb82cc293ddbeaf4c
Matias <matias@loesen.co>
2022-06-18 09:34:53 -0500

    Update Favicons.js

--- components/Favicons.js
+++ components/Favicons.js
@@ -41,18 +41,58 @@ export default function Favicons() {
         sizes='152x152'
         href='/favicons/apple-touch-icon-152x152.png'
       />
-      <link rel='icon' type='image/png' href='/favicons/favicon-196x196.png' sizes='196x196' />
-      <link rel='icon' type='image/png' href='/favicons/favicon-96x96.png' sizes='96x96' />
-      <link rel='icon' type='image/png' href='/favicons/favicon-32x32.png' sizes='32x32' />
-      <link rel='icon' type='image/png' href='/favicons/favicon-16x16.png' sizes='16x16' />
-      <link rel='icon' type='image/png' href='/favicons/favicon-128.png' sizes='128x128' />
+      <link
+        rel='icon'
+        type='image/png'
+        href='/favicons/favicon-196x196.png'
+        sizes='196x196'
+      />
+      <link
+        rel='icon'
+        type='image/png'
+        href='/favicons/favicon-96x96.png'
+        sizes='96x96'
+      />
+      <link
+        rel='icon'
+        type='image/png'
+        href='/favicons/favicon-32x32.png'
+        sizes='32x32'
+      />
+      <link
+        rel='icon'
+        type='image/png'
+        href='/favicons/favicon-16x16.png'
+        sizes='16x16'
+      />
+      <link
+        rel='icon'
+        type='image/png'
+        href='/favicons/favicon-128.png'
+        sizes='128x128'
+      />
       <meta name='application-name' content='&nbsp;' />
       <meta name='msapplication-TileColor' content='#FFFFFF' />
-      <meta name='msapplication-TileImage' content='/favicons/mstile-144x144.png' />
-      <meta name='msapplication-square70x70logo' content='/favicons/mstile-70x70.png' />
-      <meta name='msapplication-square150x150logo' content='/favicons/mstile-150x150.png' />
-      <meta name='msapplication-wide310x150logo' content='/favicons/mstile-310x150.png' />
-      <meta name='msapplication-square310x310logo' content='/favicons/mstile-310x310.png' />
+      <meta
+        name='msapplication-TileImage'
+        content='/favicons/mstile-144x144.png'
+      />
+      <meta
+        name='msapplication-square70x70logo'
+        content='/favicons/mstile-70x70.png'
+      />
+      <meta
+        name='msapplication-square150x150logo'
+        content='/favicons/mstile-150x150.png'
+      />
+      <meta
+        name='msapplication-wide310x150logo'
+        content='/favicons/mstile-310x150.png'
+      />
+      <meta
+        name='msapplication-square310x310logo'
+        content='/favicons/mstile-310x310.png'
+      />
     </>
   );
 }
